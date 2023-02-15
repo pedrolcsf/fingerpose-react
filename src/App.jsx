b4376df794076a0370c4e49bf026a89d469db6ba
Pedro Ferreira <pedrolcsferreira@gmail.com>
2023-02-15 19:57:57 -0400

    app handpose complete and init a config fingerpose

---
+++ src/App.jsx
@@ -0,0 +1,79 @@
+import {useRef, useState} from 'react'
+import * as tf from '@tensorflow/tfjs'
+import * as handpose from '@tensorflow-models/handpose'
+import Webcam from 'react-webcam'
+import { drawHand } from './utilities'
+
+function App() {
+  const webcamRef = useRef(null)
+  const canvasRef = useRef(null)
+
+  const runHandpose = async () => {
+    const net = await  handpose.load()
+    console.log('handpose model loaded')
+
+    setInterval(() => {
+      detect(net)
+    }, 100)
+  }
+
+  const detect = async (net) => {
+    if(
+      typeof webcamRef.current !== "undefined" &&
+      webcamRef.current !== null &&
+      webcamRef.current.video.readyState === 4
+    ){
+      const video = webcamRef.current.video
+      const videoWidth = webcamRef.current.video.videoWidth
+      const videoHeight = webcamRef.current.video.videoHeight
+
+      webcamRef.current.video.width = videoWidth
+      webcamRef.current.video.height = videoHeight
+
+      canvasRef.current.width = videoWidth
+      canvasRef.current.height = videoHeight
+
+      const hand = await net.estimateHands(video)
+      const ctx = canvasRef.current.getContext("2d")
+      drawHand(hand, ctx)
+    }
+  }
+
+  runHandpose()
+  return (
+    <div>
+      <header>
+      <Webcam
+        ref={webcamRef}
+        style={{
+          position: 'absolute',
+          marginLeft: 'auto',
+          marginRight: 'auto',
+          left: 0,
+          right: 0,
+          textAlign: 'center',
+          zIndex: 9,
+          width: 640,
+          height: 480
+        }}
+      />
+      <canvas
+        ref={canvasRef}
+        style={{
+          position: 'absolute',
+          marginLeft: 'auto',
+          marginRight: 'auto',
+          left: 0,
+          right: 0,
+          textAlign: 'center',
+          zIndex: 9,
+          width: 640,
+          height: 480
+        }}
+       />
+       </header>
+    </div>
+  )
+}
+
+export default App
